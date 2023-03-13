55c04176d4b6c81566519536612e80650e01d059
Søren Eskildsen <ses@trifork.com>
2023-03-13 18:44:20 +0100

    Lektion 23

--- Person.cs
+++ Person.cs
@@ -1,3 +1,46 @@
 interface Person {
-    
+    void Show(Island TheIsland, Point TheLocation);
 }
+
+class CovidPerson : Person
+{
+    static int heads = 1;
+
+    enum Situation {
+        Healthy,
+        Sick,
+        HealthyImun,
+        VacinatedImun
+    }
+
+    private Situation covidStatus;
+
+    public CovidPerson() {
+        covidStatus = CovidPerson.Situation.Healthy;
+    }
+    
+    public void GetSick(){
+        covidStatus = CovidPerson.Situation.Sick;
+    }
+    public void Show(Island TheIsland, Point TheLocation) {
+        ConsoleColor theColor;
+        switch(covidStatus) 
+        {
+            case CovidPerson.Situation.Healthy:
+                theColor = ConsoleColor.Green;
+            break;
+            case CovidPerson.Situation.Sick:
+                theColor = ConsoleColor.DarkRed;
+            break;
+            default:
+                theColor = ConsoleColor.White;
+            break;
+        }
+        TheIsland.DrawElement(TheLocation, this, theColor);
+    }
+
+    public override String ToString()
+    {
+        return "#";
+    }
+} 
--- Program.cs
+++ Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("This is a Covid-19 simulator made by Tanja");
+        SquaredIsland MyWorld ;
+        MyWorld = new SquaredIsland();
+        MyWorld.Show();
+
+        CovidPerson MyPerson;
+        MyPerson = new CovidPerson();
+        MyPerson.Show(MyWorld, new Point(10,10));
+
+        MyPerson.GetSick();
+        MyPerson.Show(MyWorld, new Point(15,15));
+
+        Console.ReadKey();
     }
 }
