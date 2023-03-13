b0742f25b7ff3ad00e380c8c5fdbdb9227b25b86
Søren Eskildsen <ses@trifork.com>
2023-03-13 18:46:07 +0100

    Lektionn24

--- Population.cs
+++ Population.cs
@@ -1,4 +1,12 @@
 interface Population
 {
-    
+    public int Size { get; }
+    //public void AddPerson(Person ThePerson);
+}
+
+class SmallPopulation : Population
+{
+    public int Size {
+        get => 0;
+    }
 }
