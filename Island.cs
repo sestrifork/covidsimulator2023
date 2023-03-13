617757f0ecd0f62ece48fe97a38f9d23f0da59d9
sestrifork <ses@trifork.com>
2023-03-13 18:37:59 +0100

    After lection 20230313

--- Island.cs
+++ Island.cs
@@ -1,4 +1,23 @@
 interface Island
 {
+    void Show();
+    void DrawElement(Point Location, Object Value);
+    void DrawElement(Point Location, Object Value, ConsoleColor color);
     
+}
+
+class SquaredIsland : ConsoleCanvas, Island
+{
+    SmallPopulation IslandPopulation;
+
+    public SquaredIsland() : base() 
+    {   
+        IslandPopulation = new SmallPopulation(); 
+    }
+
+    public void Show()
+    {
+        DrawMaxFrame();
+        DrawElement(new Point(1,1), "Squared Island World. Population size:"+IslandPopulation.Size);
+    }
 }
